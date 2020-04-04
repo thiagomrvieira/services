ee5a7bb7c4b8b5b318673fd8963c80b08b5c423e
Thiago Maciel <thiagomrvieira@gmail.com>
2020-04-04 19:15:38 +0100

    Registrando e invocando um serviço

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+App::bind('LogExemplo', function($app){
+    return 'Log registrado';
+});
+
+$exemplo = App::make('LogExemplo');
+
+dd($exemplo);
